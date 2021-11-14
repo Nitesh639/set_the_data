5c716d214015741c4ab417f411973411373f5a0a
Nitesh639 <66905080+Nitesh639@users.noreply.github.com>
2021-11-13 23:04:27 -0600

    missing_value handelling

--- missing_value.py
+++ missing_value.py
@@ -1,10 +1,33 @@
 import pandas as pd
 data = pd.read_csv(r'C:\Users\Lenovo\PycharmProjects\set_the_data\melb_data.csv')
-print(data.shape)
-#print(data.head())
-
-print(data.info())
 
 
-l=data.isnull().sum()
-print(l)
+#In this code we drop the all missing columns
+miss = []
+for col in data.columns:
+    if data[col].isnull().any():
+        miss.append(col)            #all columns with the drop values
+data1 = data.drop(miss , axis=1)        #now we drop the missing values
+print(data1.shape)
+
+#We use the fillna method to add the data
+data2 = data.copy()
+data2.fillna(method='ffill',inplace=True)
+data2.fillna(method="bfill",inplace=True)
+print(data2.shape)
+
+#It's Extension of last one
+data3 = data.copy()
+for col in miss:
+    data3[col+'_was_missing'] = data3[col].isnull()
+
+data3.fillna(method='ffill',inplace=True)
+data3.fillna(method="bfill",inplace=True)
+print(data3.shape)
+
+from sklearn.impute import SimpleImputer
+data4 = data.copy()
+mean_imputer = SimpleImputer(strategy='constant')
+data5 = pd.DataFrame(mean_imputer.fit_transform(data4))
+
+# print(data5.groupby('car'))
